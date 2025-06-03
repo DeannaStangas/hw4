e9ca462f3478e6772f0326ba35f7b0af5cff9d58
Deanna Stangas <deanna.stangas@kellogg.northwestern.edu>
2025-06-03 19:09:09 +0000

    updating flash in view

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,27 +4,22 @@ class SessionsController < ApplicationController
   end
 
   def create
-    # authenticate the user
-    # 1. try to find the user by their unique identifier
-    @user = User.find_by({ "email" => params["email"] })
-    # 2. if the user exists -> check if they know their password
-    if @user != nil
-      # 3. if they know their password -> login is successful
-      if BCrypt::Password.new(@user["password"]) == params["password"]
-        session["user_id"] = @user["id"]
-        flash["notice"] = "Welcome, #{@user["username"]}."
-        redirect_to "/places"
-      else
-        # 4b. if the user doesn't know their password -> login fails
-        flash["notice"] = "Nope."
-        redirect_to "/login"
-      end
+  @user = User.find_by({ "email" => params["email"] })
+
+  if @user
+    if BCrypt::Password.new(@user["password"]) == params["password"]
+      session["user_id"] = @user["id"]
+      flash["notice"] = "Welcome, #{@user["username"]}."
+      redirect_to "/places"
     else
-      # 4a. if the user doesn't exist -> login fails
-      flash["notice"] = "Nope."
+      flash["notice"] = "Incorrect password."
       redirect_to "/login"
     end
+  else
+    flash["notice"] = "Email not found."
+    redirect_to "/login"
   end
+end
 
   def destroy
     # logout the user
